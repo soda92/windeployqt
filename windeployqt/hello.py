b195b969d9d769f4fd400fb26b9987804332c4cd
sodacris <wjxa20152015@gmail.com>
2025-02-02 21:52:15 +0800

    add directory as arg

--- windeployqt/hello.py
+++ windeployqt/hello.py
@@ -2,17 +2,20 @@ import os
 import glob
 from windeployqt.deploy import deploy
 import argparse
+from pathlib import Path
 
 
-def main(d):
-    cwd = os.getcwd()
-    files = glob.glob("**/*.exe", recursive=True, root=cwd)
+def get_all_exe(d):
+    files = glob.glob("**/*.exe", recursive=True, root_dir=str(d))
     files = list(files)
+    return files
 
+
+def choose(files):
     print("please select:")
     for i, file in enumerate(files):
         print(f"[{i}] {file}")
-    ranges = ", ".join([i for i in range(len(files))])
+    ranges = ", ".join([str(i) for i in range(len(files))])
     prompt = f"[{ranges}]: "
     selected = input(prompt)
 
@@ -28,9 +31,29 @@ def main(d):
 
     while not is_valid(selected):
         selected = input(prompt)
+    selected = int(selected)
 
-    deploy(files[selected])
+    return files[selected]
+
+
+def main(d):
+    files = get_all_exe(d)
+    if len(files) == 0:
+        print("no exe found")
+        return
+    elif len(files) == 1:
+        deploy(files[0])
+    else:
+        file = choose(files)
+        deploy(file)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--dir", "-d", type=str, default=os.getcwd(), help="project directory"
+    )
+
+    args = parser.parse_args()
+    d = Path(args.dir).resolve()
+    main(d)
