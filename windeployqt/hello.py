cd5f827ff8729462d029e550f41f2e8a7b77b187
sodacris <wjxa20152015@gmail.com>
2025-02-03 01:01:06 +0800

    add inplace deploy

--- windeployqt/hello.py
+++ windeployqt/hello.py
@@ -3,6 +3,7 @@ import glob
 from windeployqt.deploy import deploy
 import argparse
 from pathlib import Path
+import subprocess
 
 
 def get_all_exe(d):
@@ -49,28 +50,61 @@ def get_valid_files(files, d):
     return ret
 
 
-def main(d):
-    files = get_all_exe(d)
-    files = get_valid_files(files, d)
-    if len(files) == 0:
-        print("no exe found")
-        return
-    elif len(files) == 1:
-        deploy(files[0], d)
-    else:
-        file = choose(files)
-        deploy(file, d)
-
-
 def main2():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--dir", "-d", type=str, default=os.getcwd(), help="project directory"
     )
+    parser.add_argument(
+        "--inplace",
+        "-I",
+        action="store_true",
+        default=False,
+        help="deploy inplace (suit for debug-and-build)",
+    )
+
+    parser.add_argument(
+        "--symlink",
+        "-s",
+        action="store_true",
+        default=False,
+        help="create desktop shortcut",
+    )
+
+    parser.add_argument(
+        "--open", "-o", action="store_true", default=False, help="open build dir"
+    )
 
     args = parser.parse_args()
     d = Path(args.dir).resolve()
-    main(d)
+    inplace = args.inplace
+
+    files = get_all_exe(d)
+    files = get_valid_files(files, d)
+    file = ""
+    if len(files) == 0:
+        print("no exe found")
+        return
+    elif len(files) == 1:
+        file = files[0]
+    else:
+        file = choose(files)
+
+    file = Path(file)
+    destdir = None
+    if inplace:
+        destdir = Path(file).resolve().parent
+    else:
+        destdir = Path(d).joinpath("dist")
+    deploy(file, d, destdir)
+
+    if args.open:
+        subprocess.Popen(f"explorer {destdir}")
+
+    if args.symlink:
+        from windeployqt.desktop import create_shortcut
+
+        create_shortcut(file, file.name)
 
 
 if __name__ == "__main__":
